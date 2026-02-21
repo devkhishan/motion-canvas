8dd627dbc1c5291678d99183a4eb92b9592c58ce
devkhishan <devk21111.cd@rmkec.ac.in>
2026-02-21 23:05:07 +0530

    yield keyword

--- src/scenes/example.tsx
+++ src/scenes/example.tsx
@@ -2,14 +2,12 @@ import { Circle, makeScene2D } from "@motion-canvas/2d";
 import { all, createRef } from "@motion-canvas/core";
 
 export default makeScene2D(function* (view) {
-  const myCircle = createRef<Circle>();
+  const circle = createRef<Circle>();
+  view.add(<Circle ref={circle} width={100} height={100} />);
 
-  view.add(
-    <Circle ref={myCircle} x={0} width={140} height={140} fill="#e13238" />,
-  );
-
-  yield* all(
-    myCircle().position.x(300, 2).to(-300, 2),
-    myCircle().fill("#e6a700", 1).to("#e13238", 1),
-  );
+  circle().fill("red");
+  yield;
+  circle().fill("blue");
+  yield;
+  circle().fill("red");
 });
