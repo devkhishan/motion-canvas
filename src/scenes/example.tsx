8109fa4e7a86a1cece14bfeb3fc8c2770de74d91
devkhishan <devk21111.cd@rmkec.ac.in>
2026-02-22 20:57:38 +0530

    multiple WaitFor() in single scene

--- src/scenes/example.tsx
+++ src/scenes/example.tsx
@@ -1,13 +1,18 @@
 import { Circle, makeScene2D } from "@motion-canvas/2d";
-import { all, createRef } from "@motion-canvas/core";
+import { ThreadGenerator, createRef, waitFor } from "@motion-canvas/core";
 
 export default makeScene2D(function* (view) {
   const circle = createRef<Circle>();
   view.add(<Circle ref={circle} width={100} height={100} />);
 
-  circle().fill("red");
-  yield;
-  circle().fill("blue");
-  yield;
-  circle().fill("red");
+  yield* flicker(circle());
 });
+
+function* flicker(circle: Circle): ThreadGenerator {
+  circle.fill("red");
+  yield* waitFor(1);
+  circle.fill("blue");
+  yield* waitFor(1);
+  circle.fill("red");
+  yield* waitFor(1);
+}
