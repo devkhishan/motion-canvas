47bd27a93c57b40bed0cc24f6027e432cb33db28
devkhishan <devk21111.cd@rmkec.ac.in>
2026-02-22 20:55:43 +0530

    Adding multiple scenes

--- src/project.ts
+++ src/project.ts
@@ -1,7 +1,7 @@
-import {makeProject} from '@motion-canvas/core';
+import { makeProject } from "@motion-canvas/core";
 
-import example from './scenes/example?scene';
-
+import example from "./scenes/example?scene";
+import loop from "./scenes/loop?scene";
 export default makeProject({
-  scenes: [example],
+  scenes: [example, loop],
 });
